5f1b9e911268d2dcd70ebf95ee8aa4ad585bef80
Jason <joohyuk.yang00@gmail.com>
2021-12-14 20:07:44 +0900

    feat: views 구조 구현
    더미 이미지 및 상단 네비게이션 바 적용

--- __test__/legalCheck.test.js
+++ __test__/legalCheck.test.js
@@ -5,21 +5,21 @@ const {
 } = require('../routes/controllers/legalCheck');
 
 describe('legalCheck callCntUp Test', () => {
-  it('callCntUp 함수 호출 시 resultId 파라미터가 없다면, rejects --> callCntUp Error가 발생한다.', async () => {
-    await expect(callCntUp()).rejects.toThrow('callCntUp Error');
-  });
+    it('callCntUp 함수 호출 시 resultId 파라미터가 없다면, rejects --> callCntUp Error가 발생한다.', async () => {
+        await expect(callCntUp()).rejects.toThrow('callCntUp Error');
+    });
 
-  it('callCntUp 함수 호출 시 resultId 파라미터가 있고 정상 처리되면, resolves --> true', async () => {
-    await expect(callCntUp(1)).resolves.toBe(true);
-  });
+    it('callCntUp 함수 호출 시 resultId 파라미터가 있고 정상 처리되면, resolves --> true', async () => {
+        await expect(callCntUp(1)).resolves.toBe(true);
+    });
 });
 
 describe('legalCheck calcResultType Test', () => {
     it('calcResultType 함수 호출 시 checkVal 파라미터가 없다면 --> undefined 를 리턴한다.', () => {
-      expect(calcResultType()).toEqual(undefined);
+        expect(calcResultType()).toEqual(undefined);
     });
-  
+
     it('calcResultType 함수 호출 시 checkVal 파라미터가 있고 정상 처리되면, String이 담긴 배열을 리턴한다.', () => {
-      expect(calcResultType('0000')).toEqual(expect.arrayContaining([expect.any(String)]));
+        expect(calcResultType('0000')).toEqual(expect.arrayContaining([expect.any(String)]));
     });
-  });
+});
