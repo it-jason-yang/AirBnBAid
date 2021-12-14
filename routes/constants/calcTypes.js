8152350943174bde40eaa96fd2f6a06d0e5ce452
Jason <joohyuk.yang00@gmail.com>
2021-12-14 22:11:44 +0900

    fix: calcTypes.js 파일 내 객체 초기값 설정 오류 수정 = => :

--- routes/constants/calcTypes.js
+++ routes/constants/calcTypes.js
@@ -1,14 +1,14 @@
 const calcTypes = {
-    calcType1 = ['0000','0001'],
-    calcType2 = ['0010', '0011', '0110', '0111', '0200', '0210', '0211'],
-    calcType3 = ['0100'],
-    calcType4 = ['0101'],
-    calcType5 = ['0201'],
-    calcType6 = ['1000', '2000'],
-    calcType7 = ['1001', '2001'],
-    calcType8 = ['1100', '2100'],
-    calcType9 = ['1101', '2101'],
-    calcType10 = ['1201', '2201']
+    calcType1 : ['0000','0001'],
+    calcType2 : ['0010', '0011', '0110', '0111', '0200', '0210', '0211'],
+    calcType3 : ['0100'],
+    calcType4 : ['0101'],
+    calcType5 : ['0201'],
+    calcType6 : ['1000', '2000'],
+    calcType7 : ['1001', '2001'],
+    calcType8 : ['1100', '2100'],
+    calcType9 : ['1101', '2101'],
+    calcType10 : ['1201', '2201']
 }
 
 module.exports = calcTypes;
