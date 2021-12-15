9331273e86a759d6d4a9333c4db515cfb2202f32
Jason <joohyuk.yang00@gmail.com>
2021-12-16 00:18:26 +0900

    fix: mobile 화면 상단 nav-bar script 코드 누락된 부분 적용

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'AirBnB Aid' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'AirBnB Aid - About ABBA' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'AirBnB Aid - Contact' });
+});
+
+router.get('/report', function(req, res, next) {
+  res.render('report', { title: 'AirBnB Aid - Report' });
+});
+
+
 module.exports = router;
