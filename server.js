69c26b04a227506965dfffcf925e123af8cbfdf9
jason <joohyuk.yang00@gmail.com>
2022-01-02 00:55:41 +0900

    update: 이미지파일 5mb초과 시 알림

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 const port = process.env.EXPRESS_PORT;
-const vhost = require('vhost');
-
-app.use(vhost('greenaid.site', app));
 
 app.listen(port, () => {
   console.log(`${port} 포트에서 서버가 정상적으로 가동되었습니다.`);
